1d018dd64f2e74b6e809a88e4037f271b1f42361
mohraf <amosngok@gmail.com>
2022-02-01 21:35:30 +0300

    config to production

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from .config import DevConfig
+from .config import DevConfig,ProdConfig
 from flask_bootstrap import Bootstrap
 
 # Initializing application
 app = Flask(__name__, instance_relative_config=True)
 
 # Setting up configuration
-app.config.from_object(DevConfig)
+app.config.from_object(ProdConfig)
 app.config.from_pyfile('config.py')
 
 # Initializing flask extensions
